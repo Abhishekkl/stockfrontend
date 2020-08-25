54f47d5f287bf1d88afa33bc56c25d4e223e0312
NvisionX <info.nvisionx@gmail.com>
2020-08-25 10:29:00 +0530

    stock frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { ChartComponent } from './chart/chart.component';
+// import stock from 'highcharts/modules/stock.src';
+// import more from 'highcharts/highcharts-more.src';
+import { ChartModule, HIGHCHARTS_MODULES } from 'angular-highcharts';
+import { StockchartComponent } from './stockchart/stockchart.component';
+import { ChartsModule } from 'ng2-charts';
+import { CompanyComponent } from './company/company.component';
+// export function highchartsModules() {
+//   // apply Highcharts Modules to this array
+//   return [stock, more];
+// }
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockchartComponent,
+    CompanyComponent
   ],
   imports: [
+    ChartsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
